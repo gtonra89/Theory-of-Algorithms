29593a49f1260540c7bda1ce84ba1f87c0ee5ecf
gtonra89 <gtonra89@gmail.com>
2018-03-13 16:11:26 +0000

    completed 4th problem

--- Problem 4/problem4.rkt
+++ Problem 4/problem4.rkt
@@ -2,18 +2,20 @@
 
 (define (sublsum? l)
   ; Combinations is a built in racket function that returns every combination of elements in a list
+
   ; cdr is used to just return the 2nd element of the list pair due to the 1st element being null
-  ; using filter, another racket func, to list out only the sublists that are true, in this case, sum of each sublist is equal to zero
+
+  ; using filter func to list only the sublists that are true, the sum of each sublist is equal to 0 this time
   (filter sum (cdr (combinations l))))
 
-; Function used to calculate sum of elements in each sublist returned from combinations
+; declare Function used to calculate sum of elements in each sublist returned from combinations
 (define (sum li)
   (if (null? li) ; if list = null then..
       '() ; return null, otherwise...
-      ; 'apply' is a racket function used to calculate the sum of elements in a list
+      ; The 'apply' racket function used to calculate the sum of elements in a list
       (equal? (apply + li) 0))) ; then check if sum is equal to 0 using 'equal' function
 
 ; Pass in the list into our function
-(sublsum? (list 1 2 3 4 -5)) ; expected result: (2 3 -5) (1 4 -5)
+(sublsum? (list 1 2 3 4 -5)) 
 
-(sublsum? (list 1 2 3 4 5)) ; expected result: '(())
+(sublsum? (list 1 2 3 4 5)) 
